837a6615f6ea4a8d756150048e063c0e165347d9
Eneilac <eduarneila@gmail.com>
2023-11-27 23:07:46 +0100

    express server  funcionando

---
+++ ejercicio1/express-server/app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const cors = require('cors');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid'); // Importar la función de generación de UUID
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const datosFichero = "db/datos.json";
+let datos = [];
+
+try {
+  const datosLeidos = fs.readFileSync(datosFichero, 'utf-8');
+  datos = JSON.parse(datosLeidos);
+} catch (error) {
+  console.error('Error al leer el archivo:', error);
+}
+
+// Función para generar una ID única
+function generateUniqueId() {
+    return uuidv4();
+}
+
+app.listen(3000, () => console.log("El servidor está escuchando en el puerto 3000"));
+
+app.get('/datos', (req, res, next) => {
+    res.json(datos);
+});
+
+app.post('/datos', (req, res) => {
+    const nuevosDatos = req.body;
+
+    if (nuevosDatos) {
+        // Generar una ID única y agregarla a los nuevos datos
+        nuevosDatos.id = generateUniqueId();
+
+        datos.push(nuevosDatos);
+        fs.writeFileSync(datosFichero, JSON.stringify(datos, null, 2));
+        res.json({ mensaje: 'Datos agregados correctamente', id: nuevosDatos.id });
+    } else {
+        res.status(400).json({ error: 'Datos no válidos en la solicitud' });
+    }
+});
--- ejercicio1/js/script.js
+++ ejercicio1/js/script.js
@@ -1,30 +1,42 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const formulario = document.getElementById('miFormulario'); 
-  
-    formulario.addEventListener('submit', function (event) {
-      event.preventDefault(); // Evita el comportamiento por defecto del formulario (recargar la página)
-  
-      const formData = new FormData(formulario);
-  
-      // Realiza la petición POST utilizando Fetch
-      fetch('/guardar-datos', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(Object.fromEntries(formData)),
+  const formulario = document.getElementById('miFormulario');
+
+  formulario.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    let formData = {
+      nombre: formulario.elements["nombre"].value,
+      equipo: formulario.elements["equipo"].value,
+      pais: formulario.elements["pais"].value,
+      habilidades: {
+        pac: formulario.elements["pac"].value,
+        sho: formulario.elements["sho"].value,
+        pas: formulario.elements["pas"].value,
+        dri: formulario.elements["dri"].value,
+        def: formulario.elements["def"].value,
+        phy: formulario.elements["phy"].value,
+      },
+      imagen: formulario.elements["img"].value,
+    };
+
+    console.log('Datos a enviar:', formData);
+
+    fetch('http://localhost:3000/datos', {
+      method: 'POST',
+      mode: 'cors',
+      credentials: 'same-origin',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Respuesta del servidor:', data);
       })
-        .then(response => response.json())
-        .then(data => {
-          console.log('Respuesta del servidor:', data);
-          // Puedes realizar acciones adicionales aquí, como actualizar la interfaz de usuario, mostrar un mensaje, etc.
-        })
-        .catch(error => {
-          console.error('Error al realizar la solicitud:', error);
-          // Maneja errores y realiza acciones adicionales si es necesario
-        });
-    });
+      .catch(error => {
+        console.error('Error al realizar la solicitud:', error);
+      });
   });
-  
-
-
+});
